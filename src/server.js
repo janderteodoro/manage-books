d23c63dd92f0733768eea3511cdb1c2f2857449f
Jander Teodoro <jandertheodoro@gmail.com>
2024-05-27 20:21:42 -0300

    chore: crate server file

--- src/server.js
+++ src/server.js
@@ -9,28 +9,4 @@ app.use(express.json())
 
 app.use(routes)
 
-// app.get('/books', controller.controllerBooks.getAllBooks)
-
-// app.get('/books/:id', controller.controllerBooks.getOneBook)
-
-// app.post('/books', controller.controllerBooks.createBook)
-
-// app.patch('/books/:id', controller.controllerBooks.updateBook)
-
-// app.delete('/books/:id', controller.controllerBooks.deleteBook)
-
-// app.post('/users', middlewares.validatePassword, middlewares.encryptPass, controller.controllerUsers.createUser)
-
-// app.get('/users', controller.controllerUsers.getAllUsers)
-
-// app.get('/users/:id', controller.controllerUsers.getOneUser)
-
-// app.patch('/users/:id', controller.controllerUsers.updateUser)
-
-// app.delete('/users/:id', middlewares.jwtValidate, controller.controllerUsers.deleteUser)
-
-// app.post('/users/login', middlewares.encryptPass, controller.controllerUsers.loginUser)
-
-app.listen(3000, () => {
-  console.log('server running... 🚀')
-})
+module.exports = app
